95c021b4a2c2f578c4b1b45cb40185410c9316dd
Fernando Martin <fernando_martin_martin@carrefour.com>
2020-07-11 17:25:52 +0200

    ktlint added.

--- app/src/main/java/com/example/american/main/ui/MainViewModel.kt
+++ app/src/main/java/com/example/american/main/ui/MainViewModel.kt
@@ -3,6 +3,6 @@ package com.example.american.main.ui
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
