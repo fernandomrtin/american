f14a2e61aaa155d56afd5db1a5dd73b057e4783f
Fernando Martin <fernando_martin_martin@carrefour.com>
2020-07-13 00:20:38 +0200

    Corrections on code style.

--- app/src/main/java/com/example/american/main/model/datasource/remote/service/AmericanClientServiceImpl.kt
+++ app/src/main/java/com/example/american/main/model/datasource/remote/service/AmericanClientServiceImpl.kt
@@ -4,7 +4,7 @@ import arrow.core.Either
 import com.example.american.base.network.model.CommonError
 import com.example.american.main.model.entity.SessionTokenEntity
 
-class AmericanClientServiceImpl: AmericanClientService {
+class AmericanClientServiceImpl : AmericanClientService {
     override suspend fun postDoLogin(username: String, password: String): Either<CommonError, SessionTokenEntity> {
         return Either.Right(SessionTokenEntity("12b178be172be182bce21e122v1e701"))
     }
