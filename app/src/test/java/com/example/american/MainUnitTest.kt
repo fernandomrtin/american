df11c8a0081dc2b682b069612d99a9240749ee59
Fernando Martin <fernando_martin_martin@carrefour.com>
2020-07-14 20:41:37 +0200

    Added test to check if correct login orders navigation to private area.

--- app/src/test/java/com/example/american/MainUnitTest.kt
+++ app/src/test/java/com/example/american/MainUnitTest.kt
@@ -2,7 +2,9 @@ package com.example.american
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import androidx.navigation.NavDirections
 import arrow.core.Either
+import com.example.american.base.navigation.NavigationCommand
 import com.example.american.base.network.model.CommonError
 import com.example.american.main.domain.models.SessionToken
 import com.example.american.main.domain.models.StorageSessionObject
@@ -12,13 +14,16 @@ import com.example.american.main.domain.usecase.RetrieveStoreSessionFieldsUseCas
 import com.example.american.main.domain.usecase.StoreSessionFieldsUseCase
 import com.example.american.main.model.AmericanClientRepository
 import com.example.american.main.ui.models.SessionTokenModel
+import com.example.american.main.ui.view.MainFragmentDirections
 import com.example.american.main.ui.viewmodel.MainViewModel
 import io.mockk.coEvery
 import io.mockk.mockk
+import java.util.UUID
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -33,12 +38,12 @@ class MainUnitTest {
     @get:Rule
     val coroutinesTestRule = CoroutinesTestRule()
 
-    lateinit var viewModel: MainViewModel
+    lateinit var mainViewModel: MainViewModel
 
     private val correctUser = User("fernando", "fernando")
     private val wrongUser = User("pascual", "pascual")
 
-    private val tokenCode = "9382ewfklakej329ufb2"
+    private val tokenCode = UUID.randomUUID().toString().replace("-", "")
 
     @Before
     fun setUp() {
@@ -52,24 +57,21 @@ class MainUnitTest {
         coEvery {
             repository.storeSessionFields(StorageSessionObject(correctUser.username, SessionToken(tokenCode)))
         }.returns(true)
-        coEvery {
-            repository.storeSessionFields(StorageSessionObject(wrongUser.username, SessionToken(tokenCode)))
-        }.returns(false)
         val doLoginUseCase = PostDoLoginUseCase(repository)
         val storeSessionFieldsUseCase = StoreSessionFieldsUseCase(repository)
         val retrieveStoreSessionFieldsUseCase = RetrieveStoreSessionFieldsUseCase(repository)
-        viewModel = MainViewModel(doLoginUseCase, storeSessionFieldsUseCase, retrieveStoreSessionFieldsUseCase)
+        mainViewModel = MainViewModel(doLoginUseCase, storeSessionFieldsUseCase, retrieveStoreSessionFieldsUseCase)
     }
 
     @Test
     fun `success if login with correct user is ok`() {
         val observer = mock(Observer::class.java) as Observer<in SessionTokenModel>
         coroutinesTestRule.testDispatcher.runBlockingTest {
-            viewModel.sessionToken.observeForever(observer)
+            mainViewModel.sessionToken.observeForever(observer)
             runBlocking {
-                viewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
             }
-            assertEquals(tokenCode, viewModel.sessionToken.value?.tokenCode)
+            assertEquals(tokenCode, mainViewModel.sessionToken.value?.tokenCode)
         }
     }
 
@@ -77,11 +79,28 @@ class MainUnitTest {
     fun `success if login with wrong user is ko`() {
         val observer = mock(Observer::class.java) as Observer<in SessionTokenModel>
         coroutinesTestRule.testDispatcher.runBlockingTest {
-            viewModel.sessionToken.observeForever(observer)
+            mainViewModel.sessionToken.observeForever(observer)
             runBlocking {
-                viewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, wrongUser).join()
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, wrongUser).join()
             }
-            assertEquals(true, viewModel.errorVisibility.value)
+            assertEquals(true, mainViewModel.errorVisibility.value)
+        }
+    }
+    @Test
+    fun `success if correct login orders navigation to private area`() {
+        val observer = mock(Observer::class.java) as Observer<in NavigationCommand>
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            mainViewModel.navigationCommand.observeForever(observer)
+            runBlocking {
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
+            }
+            val navigationCommandValue = mainViewModel.navigationCommand.value
+            if (navigationCommandValue is NavigationCommand.To) {
+                val directions: NavDirections = navigationCommandValue.directions
+                assertEquals(MainFragmentDirections.actionMainScreenToPrivateZoneScreen(), directions)
+            } else {
+                fail("Exception not thrown")
+            }
         }
     }
 }
