702a720f47ae3a45d07087567c833622982288bd
Fernando Martin <fernando_martin_martin@carrefour.com>
2020-07-14 20:12:54 +0200

    Added two tests to test if with a correct stored of session token, the app orders a navigation to private area and with a deletion of session token stored the app orders a navigation to login area.

--- app/src/test/java/com/example/american/MainUnitTest.kt
+++ app/src/test/java/com/example/american/MainUnitTest.kt
@@ -2,27 +2,33 @@ package com.example.american
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import androidx.navigation.NavDirections
 import arrow.core.Either
+import com.example.american.base.navigation.NavigationCommand
 import com.example.american.base.network.model.CommonError
 import com.example.american.main.domain.models.SessionToken
 import com.example.american.main.domain.models.StorageSessionObject
 import com.example.american.main.domain.models.User
 import com.example.american.main.domain.usecase.PostDoLoginUseCase
+import com.example.american.main.domain.usecase.RemoveStoreSessionFieldsUseCase
 import com.example.american.main.domain.usecase.RetrieveStoreSessionFieldsUseCase
 import com.example.american.main.domain.usecase.StoreSessionFieldsUseCase
 import com.example.american.main.model.AmericanClientRepository
 import com.example.american.main.ui.models.SessionTokenModel
+import com.example.american.main.ui.view.MainFragmentDirections
 import com.example.american.main.ui.viewmodel.MainViewModel
+import com.example.american.main.ui.viewmodel.PrivateZoneViewModel
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
+import java.util.UUID
 
 @ExperimentalCoroutinesApi
 class MainUnitTest {
@@ -33,12 +39,13 @@ class MainUnitTest {
     @get:Rule
     val coroutinesTestRule = CoroutinesTestRule()
 
-    lateinit var viewModel: MainViewModel
+    lateinit var mainViewModel: MainViewModel
+    lateinit var privateZoneViewModel: PrivateZoneViewModel
 
     private val correctUser = User("fernando", "fernando")
     private val wrongUser = User("pascual", "pascual")
 
-    private val tokenCode = "9382ewfklakej329ufb2"
+    private val tokenCode = UUID.randomUUID().toString().replace("-", "")
 
     @Before
     fun setUp() {
@@ -52,24 +59,27 @@ class MainUnitTest {
         coEvery {
             repository.storeSessionFields(StorageSessionObject(correctUser.username, SessionToken(tokenCode)))
         }.returns(true)
-        coEvery {
-            repository.storeSessionFields(StorageSessionObject(wrongUser.username, SessionToken(tokenCode)))
-        }.returns(false)
         val doLoginUseCase = PostDoLoginUseCase(repository)
         val storeSessionFieldsUseCase = StoreSessionFieldsUseCase(repository)
         val retrieveStoreSessionFieldsUseCase = RetrieveStoreSessionFieldsUseCase(repository)
-        viewModel = MainViewModel(doLoginUseCase, storeSessionFieldsUseCase, retrieveStoreSessionFieldsUseCase)
+        mainViewModel = MainViewModel(doLoginUseCase, storeSessionFieldsUseCase, retrieveStoreSessionFieldsUseCase)
+
+        coEvery {
+            repository.removeStoreSessionFields()
+        }.returns(true)
+        val removeStoreSessionFieldsUseCase = RemoveStoreSessionFieldsUseCase(repository)
+        privateZoneViewModel = PrivateZoneViewModel(removeStoreSessionFieldsUseCase)
     }
 
     @Test
     fun `success if login with correct user is ok`() {
         val observer = mock(Observer::class.java) as Observer<in SessionTokenModel>
         coroutinesTestRule.testDispatcher.runBlockingTest {
-            viewModel.sessionToken.observeForever(observer)
+            mainViewModel.sessionToken.observeForever(observer)
             runBlocking {
-                viewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
             }
-            assertEquals(tokenCode, viewModel.sessionToken.value?.tokenCode)
+            assertEquals(tokenCode, mainViewModel.sessionToken.value?.tokenCode)
         }
     }
 
@@ -77,11 +87,41 @@ class MainUnitTest {
     fun `success if login with wrong user is ko`() {
         val observer = mock(Observer::class.java) as Observer<in SessionTokenModel>
         coroutinesTestRule.testDispatcher.runBlockingTest {
-            viewModel.sessionToken.observeForever(observer)
+            mainViewModel.sessionToken.observeForever(observer)
             runBlocking {
-                viewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, wrongUser).join()
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, wrongUser).join()
             }
-            assertEquals(true, viewModel.errorVisibility.value)
+            assertEquals(true, mainViewModel.errorVisibility.value)
+        }
+    }
+    @Test
+    fun `success if correct login orders navigation to private area`() {
+        val observer = mock(Observer::class.java) as Observer<in NavigationCommand>
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            mainViewModel.navigationCommand.observeForever(observer)
+            runBlocking {
+                mainViewModel.doLoginUseCase(coroutinesTestRule.testDispatcher, correctUser).join()
+            }
+            val navigationCommandValue = mainViewModel.navigationCommand.value
+            if (navigationCommandValue is NavigationCommand.To) {
+                val directions: NavDirections = navigationCommandValue.directions
+                assertEquals(MainFragmentDirections.actionMainScreenToPrivateZoneScreen(), directions)
+            } else {
+                fail("Exception not thrown");
+            }
+        }
+    }
+
+    @Test
+    fun `success if removing session token from local orders navigation to login area`() {
+        val observer = mock(Observer::class.java) as Observer<in NavigationCommand>
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            privateZoneViewModel.navigationCommand.observeForever(observer)
+            runBlocking {
+                privateZoneViewModel.removeFieldsInLocal(coroutinesTestRule.testDispatcher).join()
+            }
+            val navigationCommandValue = privateZoneViewModel.navigationCommand.value
+            assertTrue(navigationCommandValue is NavigationCommand.Back)
         }
     }
 }
